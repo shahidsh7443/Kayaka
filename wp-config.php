c63ee688ad9b167863d6bdc7eb27b26cf8ab9f63
shahidsh7443 <shahidsh7443@gmail.com>
2018-03-30 17:09:33 +0530

    updated 30-3-2018

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@ require_once('wp-rbuilt.php');
 define('DB_NAME', 'kayaka');
 
 /** MySQL database username */
-define('DB_USER', 'shahid');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'shahidsh7443');
+define('DB_PASSWORD', 'razorbee@123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
